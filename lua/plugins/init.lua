8ec825486eb82abce3c9c1181cb7bf148d174980
Bimal <trisky650@gmail.com>
2024-02-29 23:18:38 +0530

    initial

---
+++ lua/plugins/init.lua
@@ -0,0 +1,13 @@
+return{
+    "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+
+  -- colorscheme
+  {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+}
+}
